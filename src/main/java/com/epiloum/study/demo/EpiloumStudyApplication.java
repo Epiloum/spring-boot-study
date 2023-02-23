734bf5218c62f62091c9bab4ad0a32c39bddbee8
Jaewon Kim <epiloum@gmail.com>
2023-02-24 00:59:48 +0900

    first commit

---
+++ src/main/java/com/epiloum/study/demo/EpiloumStudyApplication.java
@@ -0,0 +1,13 @@
+package com.epiloum.study.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiloumStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpiloumStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epiloum/study/demo/EpiloumStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epiloum.study.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpiloumStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
